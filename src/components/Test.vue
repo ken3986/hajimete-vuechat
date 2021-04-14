c81e4d076e6fc3653dc29a2cbf7baf9318f3febf
ken3986 <naka_ken8686@yahoo.co.jp>
2021-04-14 15:08:25 +0900

    テストページ表示＆axiosテスト了

---
+++ src/components/Test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>Test page</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data(){
+    return {
+
+    }
+  },
+
+  mounted() {
+    this.getData();
+  },
+
+  methods: {
+    async getData() {
+      const result = await axios.get('https://netlify-cors-test-ken3986.netlify.app/.netlify/functions/app');
+      console.log(result);
+    }
+  }
+}
+</script>
