3e9dc957ac886d9f2b785ad39d6aaabe28842d2a
ken3986 <naka_ken8686@yahoo.co.jp>
2021-04-18 18:43:48 +0900

    チャンネル取得 メッセージ取得 メッセージ送信機能実装

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import axios from 'axios';
+
+
+import getters from './getters';
+import mutations from './mutations';
+import actions from './actions';
 
 Vue.use(Vuex);
 
@@ -10,78 +14,14 @@ const state = {
   channels: [],
 }
 
-const getters = {
-  messages (state) {
-    return state.messages.reverse();
-  },
-  channels (state) {
-    return state.channels;
-  }
-}
-
-const mutations = {
-  getTest(state, testMessage) {
-    state.test = testMessage;
-  },
-  // setMessage(state, message) {
-  //   state.messages.push(message);
-  // },
-
-  getChannels(state, channels) {
-    state.channels = channels;
-  },
-
-  setMessages(state, messages) {
-    state.messages = messages;
-  }
-}
-
-// const get_message_path = cname => `http://localhost:9000/.netlify/functions/express/channels/${cname}/messages`;
-// const get_message_path = cname => `https://hajimete-vuechat-api-develop.netlify.app/.netlify/functions/express/channels/${cname}/messages`;
-// require('dotenv').config({path: __dirname + '/.env'});
-
-const get_message_path = cname => process.env.VUE_APP_API_URL + `/channels/${cname}/messages`;
-
-const fetchGetMessages = async(cname) => {
-  const result = await axios.get(get_message_path(cname));
-  const messages = result.data.messages;
-  return messages;
-}
-
-const actions = {
-  getTest(context) {
-    context.commit('getTest', 'testtest');
-  },
-
-  // setMessage (context, message) {
-  //   context.commit('setMessage', message);
-  // },
-
-  async getChannels(context) {
-    const result = await axios.get(process.env.VUE_APP_API_URL + '/channels');
-    const channels = result.data.channels;
-    context.commit('getChannels', channels);
-  },
-
-  async getMessages(context, cname) {
-    const messages = await fetchGetMessages(cname);
-    context.commit('setMessages', messages);
-  },
-
-  async postMessage(context, {cname, message}) {
-    const result = await axios.post(get_message_path(cname), {
-      'body': message
-    });
-    if(result.status == 201) {
-      const messages = await fetchGetMessages(cname);
-      context.commit('setMessages', messages);
-    }
-  }
-}
-
 export default new Vuex.Store({
   state,
   getters,
   mutations,
   actions,
 });
+
+
+
+
+
